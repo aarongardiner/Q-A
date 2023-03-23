feaeea28be88c3593e77ee96df6f6691e9af8258
Aaron Gardiner <126822271+aarongardiner@users.noreply.github.com>
2023-03-23 20:08:19 +0000

    i made the point system into a function to store the points in, and tells the user the points / 10

--- main.py
+++ main.py
@@ -1,23 +1,33 @@
 #This is where the questions and answers are stored.
 questions = ["what main classes are in A block?", " what main classes are in B block?", " what main classes are in C block", " what main classes are in D block", " what main classes are in E block", "What is the main subject studied in F-block?", "what main classes are in G block?", "what main classes are in H block?", "what main classes are in K block?", "what main classes are in M block?", ] #questions variable
 answers = ["Art", "Tech", "Math", "Social Studies", "Tech", "Science", "English", "Food", "Languages", "Music"] #answers variable 
+points = 0
+def start():
+  print("Welcome to my quiz on what subjects are in each block at RHS.") #prints the introduction to the quiz
 
+start()
 
-def function():
+def function():#makes the function for correct
+  print("Correct!")
+
+def point(): #makes the point function and stores the points
+    #
+    return 1 
+    
 #Code for the questions
-  point = 0   
 
-  print("Welcome to my quiz on what subjects are in each block at RHS")
+for i in range(len(questions)):
+    print("Question", i+1, "-", questions[i]) #prints the next question
+    user_answer = input("Answer - ") #tells the user to answer the question
+    if user_answer.lower() == answers[i].lower(): 
+       function() #shows the user they got it correct
+       points = points + point() # sends 1 point to the point function
+    else:
+        print("Incorrect, the correct answer is", answers[i])    #doesn't add 1 to the points because you got it incorrect, and also shows the correct answer to the user.
 
-  for i in range(len(questions)):
-      print("Question", i+1, "-", questions[i]) #prints the next question
-      user_answer = input("Answer - ") #tells the user to answer 
-      if user_answer.lower() == answers[i].lower(): 
-          print("Correct!") #shows the user they got it correct and adds 1 point
-          point = point + 1   
-      else:
-          print("Incorrect, the correct answer is", answers[i])    #doesn't add 1 to the points because you got it incorrect, and also shows the correct answer to the user.
+print("Your total points are:", points, "/", len(questions))    #calculates the amount of questions you got correct and how many you got incorrect.
 
-  print("Your total points are:", point, "/", len(questions))    #calculates the amount of questions you got correct and how many you got incorrect.
-
-input (function())
+def end():
+  print("Thanks for playing!")
+  
+end()  #overcomplication of printing the ending
